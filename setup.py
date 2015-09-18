69034b7d5abc813725d7abf5ad773d14819eca9f
Leopoldo Parra <lparra@mediexcel.com>
2015-09-18 09:54:04 -0700

    Updated setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,15 @@ from distutils.core import setup
 
 setup(
     name='django-dajax-me',
-    version='0.9.3',
+    version='0.9.4',
     author='Jorge Bastida',
     author_email='me@jorgebastida.com',
+    maintainer='Leopoldo Parra',
+    maintainer_email='lparra.dev@gmail.com',
     description=('Easy to use library to create asynchronous presentation '
                  'logic with django and dajaxice'),
-    url='http://dajaxproject.com',
+    url='https://github.com/Leonime/django-dajax.git',
+    download_url='https://github.com/Leonime/django-dajax/tarball/0.9.3',
     license='BSD',
     packages=['dajax'],
     package_data={'dajax': ['static/dajax/*']},
@@ -15,16 +18,17 @@ setup(
                       'develop asynchronous presentation logic in web '
                       'applications using python and almost no JS code. It '
                       'supports up to four of the most popular JS frameworks: '
-                      'jQuery, Prototype, Dojo and mootols.'),
+                      'jQuery, Prototype, Dojo and mootols.'
+                      'This is a fork I made to add support for Django 1.8'),
     install_requires=[
-        'django-dajaxice>=0.5'
+        'django-dajaxice-me>=0.8'
     ],
     classifiers=['Development Status :: 4 - Beta',
-                'Environment :: Web Environment',
-                'Framework :: Django',
-                'Intended Audience :: Developers',
-                'License :: OSI Approved :: BSD License',
-                'Operating System :: OS Independent',
-                'Programming Language :: Python',
-                'Topic :: Utilities']
+                 'Environment :: Web Environment',
+                 'Framework :: Django',
+                 'Intended Audience :: Developers',
+                 'License :: OSI Approved :: BSD License',
+                 'Operating System :: OS Independent',
+                 'Programming Language :: Python',
+                 'Topic :: Utilities']
 )
